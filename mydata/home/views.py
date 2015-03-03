bd15c88d5ac6fb7709e21c66bf8f0014363a2b70
Zafrir Schop <zafrir.schop@gmail.com>
2015-03-02 23:29:13 -0500

    add map to index

--- mydata/home/views.py
+++ mydata/home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.template import Context, loader
+from django.shortcuts import redirect ,render_to_response
 
+
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+
+    t = loader.get_template("home/index.html")
+    # return HttpResponse(template.render(c))
+    html = t.render(Context({}))
+    return HttpResponse(html)
