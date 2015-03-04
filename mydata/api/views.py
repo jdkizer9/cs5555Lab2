182d53151815a7810e97f16e4be5dc5fd1a01541
Zafrir Schop <zafrir.schop@gmail.com>
2015-03-04 14:56:32 -0500

    client name

--- mydata/api/views.py
+++ mydata/api/views.py
@@ -10,26 +10,26 @@ from django.contrib.auth.models import User
 from rauth import OAuth2Service
 from my_data_network_controller import DPUNetworkController
 
-
+clientName = 'james'
 
 def RegisterPrimaryClient():
-    if(not DPUNetworkController.CheckForClient('james')):
-        client = DPUNetworkController('james', '5555-2015-*****', '*********')
-        DPUNetworkController.RegisterClient('james', client)
+    if(not DPUNetworkController.CheckForClient(clientName)):
+        client = DPUNetworkController(clientName, '5555-2015-*****', '*********')
+        DPUNetworkController.RegisterClient(clientName, client)
 
 @api_view(['GET', 'POST'])         ### replaces JSONResponse(Htttpresponse)
 def smal_data_authentication(request):
     print("user_daily_mobility_segments")
 
-    dpu_client = DPUNetworkController.GetClient('james')
+    dpu_client = DPUNetworkController.GetClient(clientName)
     url = dpu_client.get_authorize_url()
-    
+
     return HttpResponseRedirect(url)
 
-@api_view(['GET', 'POST']) 
+@api_view(['GET', 'POST'])
 def small_data_callback_handler(request):
     code = request.GET['code']
-    dpu_client = DPUNetworkController.GetClient('james')
+    dpu_client = DPUNetworkController.GetClient(clientName)
 
     dpu_client.configure_access_token(code)
 
@@ -38,8 +38,8 @@ def small_data_callback_handler(request):
 
 @api_view(['GET'])         ### replaces JSONResponse(Htttpresponse)
 def user_daily_mobility_segments(request):
-    if(DPUNetworkController.GetClient('james')):
-        dpu_client = DPUNetworkController.GetClient('james')
+    if(DPUNetworkController.GetClient(clientName)):
+        dpu_client = DPUNetworkController.GetClient(clientName)
         print dpu_client.access_token
         response = dpu_client.getMobilityDailySegmentsData()
         return HttpResponse(response.json(), content_type="application/json")
@@ -48,8 +48,8 @@ def user_daily_mobility_segments(request):
 
 @api_view(['GET'])         ### replaces JSONResponse(Htttpresponse)
 def user_daily_mobility_summary(request):
-    if(DPUNetworkController.GetClient('james')):
-        dpu_client = DPUNetworkController.GetClient('james')
+    if(DPUNetworkController.GetClient(clientName)):
+        dpu_client = DPUNetworkController.GetClient(clientName)
         print dpu_client.access_token
         response = dpu_client.getMobilityDailySummaryData()
         return HttpResponse(response.json(), content_type="application/json")
@@ -58,8 +58,8 @@ def user_daily_mobility_summary(request):
 
 @api_view(['GET'])
 def user_pam_data(request):
-    if(DPUNetworkController.GetClient('james')):
-        dpu_client = DPUNetworkController.GetClient('james')
+    if(DPUNetworkController.GetClient(clientName)):
+        dpu_client = DPUNetworkController.GetClient(clientName)
         print dpu_client.access_token
         response = dpu_client.getPAMData()
         return HttpResponse(response.json(), content_type="application/json")
@@ -67,4 +67,3 @@ def user_pam_data(request):
         return HttpResponseBadRequest
 
 RegisterPrimaryClient()
-
